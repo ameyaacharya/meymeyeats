c2fd1e3cdbbcbd30782a7a7534fdfd84c8f73dad
Ameya Acharya <ameya.acharya@typeform.com>
2020-03-14 23:40:26 +0100

    Updates to the about page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,16 +16,14 @@ const AboutPage = () => (
     </div>
     
     <div className="biography-container">
-      <p><span>H</span> ey everyone! Welcome to meymeyeats.</p>
+      <p><span>H</span> ey everyone! Welcome to meymeyeats. I'm your average person who has an above-average obsession with food.</p>
 
-      <p>I'm your average person who has an above-average obsession with food. I was born and raised in New York to an Indian family, and now I live in Barcelona Spain. One thing that's constant across all these cultures is a love for good food.</p>
+      <p>I was born and raised in New York in an Indian family, and now I live in Barcelona, Spain. My dad once said that I'm always either eating or thinking about eating. I couldn't think of a truer way to describe me. In my opinion,
+      food (and drink!) is one of the main joys in life. So if we're going to spend time, energy, and (most importantly) money eating, so why don't we make sure it tastes good?</p>
       
-      <p>My dad once said that I'm always either eating or thinking about eating. I couldn't think of a truer way to describe me. In my opinion,
-      food (and drink!) is one of the main joys in life. So why waste time, energy, and (most importantly) money eating things that just... don't taste good?</p>
+      <p>I'm here to share my meals with you... because the point of food is to bring us happiness.</p>
 
-      <p> I'm here to help you make sure your meals out are amazing. Because the point of food is to bring us happiness.</p>
-
-       <p> Come join me on this ride! Follow along on instagram at @meymeyeats.</p>
+      <p> Come join me on this ride! Follow along on Instagram at <a href="https://www.instagram.com/meymeyeats/">@meymeyeats</a>.</p>
     </div>
   </div>
 )
